3bb0308594a98b4f52510ec2e4ebd323f62e8b1f
PimDoos <17709721+PimDoos@users.noreply.github.com>
2023-04-05 22:27:38 +0200

    Add logging

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
+print("[DoosBot Startup] Loading dependencies...", end="", flush=True)
 import sys
 import discord
 import logging
+
 
 import doosbot.modules
 
 from doosbot.client import DoosBotClient
 from doosbot.utils import setup_logging
 from config import DISCORD_SECRET
+print(" [ DONE ]")
 
 setup_logging()
+_LOG = logging.getLogger(__name__)
 
+_LOG.info("Loading Discord Client...")
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = DoosBotClient(intents=intents)
+
+_LOG.info("Loading DoosBot Modules...")
 doosbot.modules.init(client)
 
-
+_LOG.info("Starting DoosBot client")
 client.run(token=DISCORD_SECRET)
+
+_LOG.info("DoosBot exited")
