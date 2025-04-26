10a13cf11bdce23cadc3014cb50826cd1af5d41f
Pim <17709721+PimDoos@users.noreply.github.com>
2025-04-27 01:15:06 +0200

    Fix wiki commands

--- doosbot/modules/wikipedia.py
+++ doosbot/modules/wikipedia.py
@@ -16,7 +16,7 @@ def init(client: doosbot.client.DoosBotClient, tree: discord.app_commands.Comman
 	async def on_message(message: discord.message.Message):
 		if(message.type == discord.MessageType.reply and message.reference != None and message.reference.cached_message != None):
 			if(re.match("wat", message.content.lower())):
-				response = await handle_question(message, message.author)
+				response = await handle_question(message.reference.cached_message.content, message.author)
 				if response is None:
 					await message.reply("Wat moet ik daar nou mee?")
 				else:
@@ -25,7 +25,7 @@ def init(client: doosbot.client.DoosBotClient, tree: discord.app_commands.Comman
 	@client.listen('on_reaction_add')
 	async def on_reaction_add(reaction: discord.Reaction, user: discord.User):
 		if reaction.emoji == DoosBotEmoji.QUESTION:
-			response = await handle_question(reaction, user)
+			response = await handle_question(reaction.message.content, user)
 			if response is None:
 				await reaction.message.channel.send(f"{ user.mention } Wat moet ik daar nou mee?")
 			else:
@@ -35,7 +35,7 @@ def init(client: doosbot.client.DoosBotClient, tree: discord.app_commands.Comman
 	async def context_menu_get_article(interaction: discord.Interaction, message: discord.Message):
 		"""Context menu command to search for a wiki article"""
 		_LOG.info(f"{ interaction.command.name } command executed by { interaction.user.name }")
-		response = await handle_question(message, interaction.user)
+		response = await handle_question(message.content, interaction.user)
 		if response is None:
 			await interaction.response.send_message(f"Wat moet ik daar nou mee?")
 		else:
@@ -45,32 +45,18 @@ def init(client: doosbot.client.DoosBotClient, tree: discord.app_commands.Comman
 	@tree.command(name="wiki", description = "Zoek op Wikipedia")
 	async def command_get_article(interaction: discord.Interaction, query: str):
 		_LOG.info(f"{ interaction.command.name } command executed by { interaction.user.name }")
-		response = await handle_question(interaction, interaction.user)
+		response = await handle_question(query, interaction.user)
 		if response is None:
 			await interaction.response.send_message(f"Wat moet ik daar nou mee?")
 		else:
 			await interaction.response.send_message(response)
 
 
-async def handle_question(trigger: discord.message.Message | discord.Reaction | discord.Interaction, user: discord.User) -> str:
+async def handle_question(query, user: discord.User) -> str:
 	"""Handle a question message and return a matching wiki article"""
-	if isinstance(trigger, discord.Reaction):
-		message = trigger.message
-		asked_by = user
-	elif isinstance(trigger, discord.Message):
-		if trigger.type == discord.MessageType.reply and trigger.reference != None and trigger.reference.cached_message != None:
-			message = trigger.reference.cached_message
-			asked_by = trigger.author
-		else:
-			return None
-	elif isinstance(trigger, discord.Interaction):
-		message = trigger.message
-		asked_by = user
-	else:
-		return None
 
-	_LOG.info(f"{ asked_by.name } is zo'n dom hondje die niet weet wat '{ message.content }' betekent")
-	wiki_article = await get_article(message.content)
+	_LOG.info(f"{ user.name } is zo'n dom hondje die niet weet wat '{ query }' betekent")
+	wiki_article = await get_article(query)
 
 	if wiki_article is None:
 		return "Daar heb ik nog nooit van gehoord"
