d922c27ad5ea6541c0dd6617370afd2a3c1530fe
Pim <17709721+PimDoos@users.noreply.github.com>
2025-12-17 19:26:24 +0100

    Add DnD stuff to dice

--- doosbot/modules/dice.py
+++ doosbot/modules/dice.py
@@ -1,5 +1,7 @@
 """Roll a dice"""
 import logging
+
+from doosbot.const import DoosBotEmoji
 _LOG = logging.getLogger(__name__)
 
 from random import randint
@@ -8,8 +10,13 @@ import doosbot.client
 
 def init(client: doosbot.client.DoosBotClient, tree: discord.app_commands.CommandTree):
 	
-	@tree.command(name="dobbeltenen", description="Rol met mijn dobbeltenen")
-	async def command_dice(interaction: discord.Interaction):
+	@tree.command(name="d", description="Rol met mijn dobbeltenen")
+	async def command_dice(interaction: discord.Interaction, die_size: int = 6, rolls: int = 1):
 		_LOG.info(f"{ interaction.command.name } command executed by { interaction.user.name }")
-		result = randint(1,6)
-		await interaction.response.send_message(f"Ik heb gerold met mijn dobbeltenen en het antwoord is: { result }")
+		roll_results = []
+		for _ in range(rolls):
+			roll = randint(1, die_size)
+			roll_results.append(roll)
+
+		await interaction.response.send_message(f" { DoosBotEmoji.DICE } Ik heb { rolls }x gedobbeld met mijn dobbeltenen ({ die_size } tenen) en dit zijn de resultaten: \n1. { '\n1. '.join(map(str, roll_results)) }")
+	
